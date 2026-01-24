c0771e8ca002d7b1d21a90088fecdb5c4611c896
tonytgrt <tonytg@seas.upenn.edu>
2026-01-24 14:23:30 -0500

    3.3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float leftBound = -10f;
+    public float rightBound = 10f;
+
+    [Header("Firing")]
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public AudioClip shootSound;
+
+    private bool canFire = true;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 newPosition = transform.position;
+        newPosition.x += horizontalInput * moveSpeed * Time.deltaTime;
+        newPosition.x = Mathf.Clamp(newPosition.x, leftBound, rightBound);
+        transform.position = newPosition;
+
+        if (Input.GetKeyDown(KeyCode.Space) && canFire)
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        if (bulletPrefab != null)
+        {
+            Vector3 spawnPos = firePoint != null ? firePoint.position : transform.position + Vector3.forward * 0.5f;
+            Instantiate(bulletPrefab, spawnPos, Quaternion.identity);
+            canFire = false;
+
+            if (shootSound != null)
+            {
+                audioSource.PlayOneShot(shootSound);
+            }
+        }
+    }
+
+    public void EnableFiring()
+    {
+        canFire = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("AlienBullet"))
+        {
+            Die();
+            Destroy(other.gameObject);
+        }
+    }
+
+    public void Die()
+    {
+        //GameManager gm = FindObjectOfType<GameManager>();
+        //if (gm != null)
+        //{
+        //    gm.PlayerHit();
+        //}
+    }
+
+
+
+}
+
+
